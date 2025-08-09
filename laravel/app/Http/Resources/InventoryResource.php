54c979809109b5c657a35151193760768af4c086
Juan Donquis <juandonquis07@gmail.com>
2025-08-09 18:42:46 -0400

    add product object in ouputs and invenvtory

--- laravel/app/Http/Resources/InventoryResource.php
+++ laravel/app/Http/Resources/InventoryResource.php
@@ -16,24 +16,31 @@ class InventoryResource extends JsonResource
     {
         // return parent::toArray($request);
 
-           return [
-                "id"=> $this->id,
-                "entity_code" => $this->entity_code,
-                "area" => $this->area,
-                "product_id"=> $this->product->id,
-                "product_code"=> $this->product->code,
-                "product_name"=> $this->product->machine,
-                "product_brand"=> $this->product->brand,
-                "product_model"=> $this->product->model,
-                "product_required_components"=> $this->product->required_components,
-                "quantity"=> $this->quantity,
-                "serial_number"=> $this->serial_number,
-                "national_code"=> $this->national_code,
-                "machine_status_id" => $this->machine_status_id,
-                "machine_status_name" => $this->machineStatus->name,
-                "components" => $this->components,
-                'last_type_maintenance_id' => $this->last_type_maintenance_id ?? null,
-                'last_type_maintenance_name' => $this->lastMaintenanceType->name ?? null,
+        return [
+            "id" => $this->id,
+            "entity_code" => $this->entity_code,
+            "area" => $this->area,
+            "product_id" => $this->product->id,
+            "product_code" => $this->product->code,
+            "product_name" => $this->product->machine,
+            "product_brand" => $this->product->brand,
+            "product_model" => $this->product->model,
+            "product_required_components" => $this->product->required_components,
+            "productObj" => (object) [
+                "name" => $this->product->machine,
+                "brand" => $this->product->brand,
+                "model" => $this->product->model,
+                "level" => $this->product->level,
+                'required_components' => $this->product->required_components,
+            ],
+            "quantity" => $this->quantity,
+            "serial_number" => $this->serial_number,
+            "national_code" => $this->national_code,
+            "machine_status_id" => $this->machine_status_id,
+            "machine_status_name" => $this->machineStatus->name,
+            "components" => $this->components,
+            'last_type_maintenance_id' => $this->last_type_maintenance_id ?? null,
+            'last_type_maintenance_name' => $this->lastMaintenanceType->name ?? null,
         ];
     }
 }
